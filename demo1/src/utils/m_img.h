cf5483a899c42c0322019feeef195b0ac57e669a
morisa66 <49175869+morisa66@users.noreply.github.com>
2021-03-13 13:37:07 +0800

    Update m_img.h

--- demo1/src/utils/m_img.h
+++ demo1/src/utils/m_img.h
@@ -17,10 +17,8 @@ unsigned m_load_img2D(const char* path)
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
-	// 环绕方式
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// 过滤方式
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
